c3445b601c9397e80328236b7188062ad0a1e441
RemartoM <Remarto_Martinez@proton.me>
2026-07-30 22:13:16 +0800

    Delete

--- обход/start_discord_bypass.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Discord DPI Bypass
-cd /d "C:\Users\Shkil\Projects"
-echo ================================
-echo  Discord DPI Bypass запускается
-echo ================================
-echo.
-python discord_bypass.py
-pause
